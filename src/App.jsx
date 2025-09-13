7dcf2a60cf40ecdbb5ea22a29ef63890126cca30
lyp <lakshmireddy.p@skillsoft.com>
2025-09-14 02:51:17 +0530

    after saving setting, home navigation issue fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,20 @@ import Settings from './pages/Settings';
 import Analysis from './pages/Analysis';
 import AnalysisDetails from './pages/AnalysisDetails';
 import PdfReader from './pages/PdfReader';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const savedConfigs = JSON.parse(localStorage.getItem('llmConfigurations') || '[]');
+  const [savedConfigs, setSavedConfigs] = useState([]);
+
+  const fetchLatestSettings = () => JSON.parse(localStorage.getItem('llmConfigurations') || '[]');
+  useEffect(() => {
+    setSavedConfigs(fetchLatestSettings());
+  }, []);
+
+  const handleSaveConfigs = () => {
+    setSavedConfigs(fetchLatestSettings());
+  };
+
 
   return (
     <Router>
@@ -21,7 +32,7 @@ function App() {
             path="/test" 
             element={<PdfReader />} 
           />
-          <Route path="/settings" element={<Settings />} />
+          <Route path="/settings" element={<Settings  onSave={handleSaveConfigs}/>} />
           <Route path="/analysis" element={<Analysis />} />
           <Route path="/analysis-details/:analysisId" element={<AnalysisDetails />} />
         </Routes>
